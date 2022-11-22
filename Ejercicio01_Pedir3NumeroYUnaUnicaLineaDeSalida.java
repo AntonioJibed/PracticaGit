a8e8f3fd237f721f19c962d29b56083bf22cfc49
AntonioJibed <antonio_jb_54@hotmail.es>
2022-11-22 12:16:57 +0100

    Feat

---
+++ Ejercicio01_Pedir3NumeroYUnaUnicaLineaDeSalida.java
@@ -0,0 +1,32 @@
+package entornos;
+
+import java.util.Scanner;
+
+/*
+1.- Realiza un programa que pida al usuario tres números: uno de tipo entero, otro de tipo flotante y otro de tipo doble.
+ A continuación se tienen que mostrar en pantalla los tres números en una sola fila de la consola de salida.
+*/
+public class Ejercicio01_Pedir3NumeroYUnaUnicaLineaDeSalida {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("Introduzca un número entero;");
+		int numEntero = sc.nextInt();
+		
+		System.out.println("Introduzca un número flotante;");
+		float numFlotante = sc.nextFloat();
+		
+		System.out.println("Introduzca un número double;");
+		double numDoble = sc.nextDouble();
+				
+		System.out.println ("El número entero es: " + numEntero +
+		". El número flotante es : " + numFlotante +
+		".El número doble es: "+ numDoble);
+		
+		
+
+	}
+
+}
