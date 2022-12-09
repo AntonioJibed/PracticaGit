3fcbfebb8206af483ca49066f4f219de3a38c930
AntonioJibed <antonio_jb_54@hotmail.es>
2022-12-09 14:35:38 +0100

    Variables

---
+++ Operaciones.java
@@ -0,0 +1,20 @@
+package entornos;
+
+import java.util.Scanner;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Primer Numero");
+		int num1 = sc.nextInt();
+		
+		System.out.println("Segundo Numero");
+		int num2 = sc.nextInt();
+		
+
+	}
+
+}
