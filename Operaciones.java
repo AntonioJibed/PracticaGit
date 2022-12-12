99a8a7172d681f9a47ccf9171be484b7b88e1f11
AntonioJibed <antonio_jb_54@hotmail.es>
2022-12-12 14:27:31 +0100

    Commit final

--- Operaciones.java
+++ Operaciones.java
@@ -7,6 +7,7 @@ public class Operaciones {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int resultado = 0;
 		
 		System.out.println("Primer Numero");
 		int num1 = sc.nextInt();
@@ -17,6 +18,18 @@ public class Operaciones {
 		System.out.println("Menu");
 		System.out.println("Pulsa 1 para sumar");
 		System.out.println("Pulsa 2 para restar");
+		int opcion = sc.nextInt();
+		
+		if (opcion == 1) {
+			resultado = num1 + num2;
+		}
+		
+		if (opcion == 2) {
+			resultado = num1 - num2;
+		}
+		
+		System.out.println("Resultado " + resultado);
+		
 
 
 	}
